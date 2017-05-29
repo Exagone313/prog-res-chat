5c0bc8b9cfbfd6d333bef5ea973491a410c80bc9
Exagone313 <exa@elou.world>
2017-05-29 11:49:02 +0200

    Handle signals to quit properly

--- src/main.c
+++ src/main.c
@@ -1,10 +1,29 @@
+#include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "usage.h"
+
+volatile int quit = 0;
+
+void signalHandler(int signal) {
+	(void) signal;
+	quit = 1;
+}
 
 int main(const int argc, const char **argv)
 {
 	if(print_usage(argc, argv))
 		return 1;
+
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
+
+	// TODO start jobs...
+
+	while(!quit)
+		pause();
+
+	// TODO terminate jobs...
 
 	printf("For clients: %ld:%d\n"
 			"For promoters: %ld:%d\n",
